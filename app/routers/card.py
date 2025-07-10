5f98632f55ff37133fde4df251610b6321fc73b4
LatteSoM <stazin228@gmail.com>
2025-07-10 18:23:44 +0300

    добавил обработку аналитики

--- app/routers/card.py
+++ app/routers/card.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, select
+from sqlmodel import Session, select, func
 from typing import List
 from ..database import get_session
-from ..models.models import Card, ContactInfo, LinkWidget
+from ..models.models import Card, ContactInfo, LinkWidget, Analytics
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -114,3 +114,33 @@ async def get_card_qr_link(card_id: int, session: Session = Depends(get_session)
     # Формируем ссылку для QR-кода
     qr_link = f"https://connectcard.ru/users/{card_id}"
     return {"qr_link": qr_link}
+
+
+
+@router.get("/{card_id}/analytics", response_model=dict)
+async def get_card_analytics(card_id: int, session: Session = Depends(get_session)):
+        # Проверка существования карточки
+        card = session.exec(select(Card).where(Card.id == card_id)).first()
+        if not card:
+            raise HTTPException(status_code=404, detail="Card not found")
+
+        # Общее количество просмотров карточки
+        total_views = session.exec(
+            select(func.count()).select_from(Analytics).where(Analytics.card_id == card_id)
+        ).one()
+
+        # Просмотры по типам устройств карточки
+        views_by_device = session.exec(
+            select(Analytics.device_type, func.count())
+            .where(Analytics.card_id == card_id)
+            .group_by(Analytics.device_type)
+        ).all()
+
+        # Формирование ответа
+        analytics_data = {
+            "card_id": card_id,
+            "total_views": total_views,
+            "views_by_device": {device_type: count for device_type, count in views_by_device}
+        }
+        
+        return analytics_data
