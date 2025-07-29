9f1ee8fd3a64c5b2979961a028d4a8e96164ab1c
memberRemember <memberremember@MacBook-Air-memberRemember.local>
2025-07-29 17:02:31 +0300

    Добавил явное приведение к типу int срока истечения действия токена

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -16,7 +16,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = os.getenv('SECRET_KEY') 
 ALGORITHM = os.getenv('ALGORITHM') 
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES') 
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30))
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
